080f74a0d4b7bb6bc9082e10bfe89f6599203cc2
Sigeti Backend <ncordoba@gaiaamazonas.org>
2024-11-01 02:12:54 -0400

    keywords added for related posts in the place for posts

--- src/data/postsUrls.ts
+++ src/data/postsUrls.ts
@@ -3,8 +3,8 @@ import type { PostsImp } from "../types/Posts";
 const postsUrl: PostsImp = {
     'post1': {title: "Post 1", route: "/posts/post-1"},
     'post2': {title: "Post 2", route: "/posts/post-2"},
-    'post3': {title: "Post 3", route: "/posts/post-3"}
-
+    'post3': {title: "Post 3", route: "/posts/post-3"},
+    'post4': {title: "Post 4", route: "/posts/post-4"}
 }
 
 export default postsUrl;
---
+++ src/pages/posts/post-5.md
@@ -0,0 +1,12 @@
+---
+layout: ../../layouts/MarkdownPostLayout.astro
+title: My fifth Blog Post
+author: Astro Learner
+description: "Not related post to test"
+image:
+    url: "https://docs.astro.build/assets/rays.webp"
+    alt: "The Astro logo on a dark background with rainbow rays."
+pubDate: 2022-07-15
+tags: ["next"]
+---
+A text without related tag to test
